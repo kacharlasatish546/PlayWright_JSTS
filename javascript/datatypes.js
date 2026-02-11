86da4428e687403ae36372f9af2cebcd18da3388
kacharlasatish <kacharlasatish546@gmail.com>
2026-02-11 11:15:59 +0530

    My First Commit on Javascript datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,42 @@
+var x=10;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x="10";
+console.log("X value is "+x+" and Type is "+typeof(x));
+x=true;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x=null;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x=undefined;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x=10.5;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x="10.5";
+console.log("X value is "+x+" and Type is "+typeof(x));
+x;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = Number("11");
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = Number("10.5");
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = Number("10 20");
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = Number("something");
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = [1, 2, 3, 4];
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = [1, 2, "something", 3, 4];
+console.log("X value is "+x+" and Type is "+typeof(x));
+
+
+var x=10;
+console.log("X value is "+x+" and Type is "+typeof(x));
+x = {firstName:"firstName", secondName:"secondName"}
+console.log("X value is "+x+" and Type is "+typeof(x));
+let a = 10
+console.log(a)
+x="120";
+console.log("X value is "+x+" and Type is "+typeof(x));
+x=Number(x)
+console.log("X value is "+x+" and Type is "+typeof(x));
+x=x/10
+console.log("X value is "+x+" and Type is "+typeof(x));
